a8692bbd293bcc7798b475ed329edcafdcbed634
Chris Tumolo <chris.tumolo@HMCTS.net>
2021-02-19 11:23:59 +0000

    Add SDS support

--- SchedulerJobs/SchedulerJobs/Service/ConfigLoader.cs
+++ SchedulerJobs/SchedulerJobs/Service/ConfigLoader.cs
@@ -2,18 +2,25 @@
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using VH.Core.Configuration;
 
 namespace SchedulerJobs.Service
 {
     public class ConfigLoader
     {
         public readonly IConfiguration Configuration;
+
         public ConfigLoader()
         {
+            const string vhInfraCore = "/mnt/secrets/vh-infra-core";
+            const string vhSchedulerJobs = "/mnt/secrets/vh-scheduler-jobs";
+
             var configRootBuilder = new ConfigurationBuilder()
                 .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
                 .AddEnvironmentVariables()
-                .AddUserSecrets<Startup>();
+                .AddUserSecrets<Startup>()
+                .AddAksKeyVaultSecretProvider(vhInfraCore)
+                .AddAksKeyVaultSecretProvider(vhSchedulerJobs);
 
             Configuration = configRootBuilder.Build();
 
