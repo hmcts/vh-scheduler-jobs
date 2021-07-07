4b6b61a86c0a12d3b18873a5f7e7e9434e157bab
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2021-07-07 15:52:34 +0100

    VIH-7938 Update the leavers endpoint to V2

--- SchedulerJobs/SchedulerJobs.Services/HttpClients/ELinksApiClient.cs
+++ SchedulerJobs/SchedulerJobs.Services/HttpClients/ELinksApiClient.cs
@@ -35,7 +35,7 @@ namespace SchedulerJobs.Services.HttpClients
         {
             var response = await _httpClient.GetAsync
             (
-                $"{BaseUrl}api/leavers?updated_since={updatedSince:yyyy-MM-dd}&page={page}&per_page={perPage}"
+                $"{BaseUrl}api/v2/leavers?left_since={updatedSince:yyyy-MM-dd}&page={page}&per_page={perPage}"
             );
 
             await HandleUnsuccessfulResponse(response);
