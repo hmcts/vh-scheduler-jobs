5e794dad868a7640f981982dfebf31d1a1a131e2
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2021-07-08 10:59:01 +0100

    Refactored to support different base Url for the Peoples and Leavers ELinks Api client

---
+++ SchedulerJobs/SchedulerJobs.Services/HttpClients/ILeaversClient.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SchedulerJobs.Common.Models;
+
+namespace SchedulerJobs.Services.HttpClients
+{
+    public interface ILeaversClient
+    {
+        Task<IEnumerable<JudiciaryPersonModel>> GetLeaversAsync(DateTime updatedSince, int page = 1, int perPage = 100);
+    }
+}
---
+++ SchedulerJobs/SchedulerJobs.Services/HttpClients/IPeoplesClient.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SchedulerJobs.Common.Models;
+
+namespace SchedulerJobs.Services.HttpClients
+{
+    public interface IPeoplesClient
+    {
+        Task<IEnumerable<JudiciaryPersonModel>> GetPeopleAsync(DateTime updatedSince, int page = 1, int perPage = 100);
+    }
+}
