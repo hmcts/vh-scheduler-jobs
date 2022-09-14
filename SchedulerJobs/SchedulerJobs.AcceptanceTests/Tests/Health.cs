193af0f8e180b7fc67d6d4c00c90c66b439a227d
Will Craig <william.craig2@justice.gov.uk>
2022-09-14 16:52:08 +0100

    Added FUNCTION_EXTENSION_VERSION to appsettings.json

--- SchedulerJobs/SchedulerJobs.AcceptanceTests/Tests/Health.cs
+++ SchedulerJobs/SchedulerJobs.AcceptanceTests/Tests/Health.cs
@@ -13,7 +13,6 @@ namespace SchedulerJobs.AcceptanceTests.Tests
         public void GetHealth()
         {
             var request = RequestHandler.Get(ApiUriFactory.HealthCheckEndpoints.CheckServiceHealth);
-            Thread.Sleep(new TimeSpan(0,0,1,0));
             var response = SendRequest(request);
             response.StatusCode.Should().Be(HttpStatusCode.OK);
             response.IsSuccessful.Should().BeTrue();
