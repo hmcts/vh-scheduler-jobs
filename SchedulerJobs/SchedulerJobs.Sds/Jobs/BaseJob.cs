0f33e32abf360714b6f1fc482e5a8f03ee677535
Oliver Scott <oliver.scott@justice.gov.uk>
2022-10-11 22:51:13 +0100

    Exclude from code coverage

--- SchedulerJobs/SchedulerJobs.Sds/Jobs/BaseJob.cs
+++ SchedulerJobs/SchedulerJobs.Sds/Jobs/BaseJob.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace SchedulerJobs.Sds.Jobs
 {
+    [ExcludeFromCodeCoverage]
     public abstract class BaseJob : BackgroundService
     {
         private readonly IHostApplicationLifetime _lifetime;
