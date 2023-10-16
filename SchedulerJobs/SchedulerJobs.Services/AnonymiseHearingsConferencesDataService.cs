8a1e3c4d29cb8cfb478ebed42814c90f43c34998
marcogagliardi <marco.gagliardi@justice.gov.uk>
2023-10-16 14:18:19 +0100

    code smell fix

--- SchedulerJobs/SchedulerJobs.Services/AnonymiseHearingsConferencesDataService.cs
+++ SchedulerJobs/SchedulerJobs.Services/AnonymiseHearingsConferencesDataService.cs
@@ -95,7 +95,7 @@ namespace SchedulerJobs.Services
             }
         }
 
-        private bool ShouldRemoveUserFromAd(UserProfile userProfile)
+        private static bool ShouldRemoveUserFromAd(UserProfile userProfile)
         {
             return userProfile.UserRole != AzureAdUserRoles.Judge &&
                    userProfile.UserRole != AzureAdUserRoles.VhOfficer &&
