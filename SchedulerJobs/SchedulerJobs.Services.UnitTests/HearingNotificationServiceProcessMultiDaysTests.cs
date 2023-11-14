c21e50357442f66f52c8fbab292f07b128074731
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-11-14 20:23:00 +0000

    VIH-9905 use new templates Refactored HearingNotificationService.cs to use the new NotificationAPI operations to simplify email template logic

---
+++ SchedulerJobs/SchedulerJobs.Services.UnitTests/HearingNotificationServiceProcessMultiDaysTests.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
+using BookingsApi.Client;
+using BookingsApi.Contract.V1.Responses;
+using FizzWare.NBuilder;
+using Moq;
+using NotificationApi.Client;
+using NotificationApi.Contract;
+using NotificationApi.Contract.Requests;
+using NUnit.Framework;
+
+namespace SchedulerJobs.Services.UnitTests;
+
+public class HearingNotificationServiceProcessMultiDaysTests
+{
+    private AutoMock _mocker;
+    private HearingNotificationService _sut;
+        
+    [SetUp]
+    public void Setup()
+    {
+        _mocker = AutoMock.GetLoose();
+        _sut = _mocker.Create<HearingNotificationService>();
+    }
+
+    [Test]
+    public async Task should_send_multi_day_reminder_request_for_supported_roles()
+    {
+        // arrange
+        var participants = Builder<ParticipantResponse>.CreateListOfSize(4)
+            .All().With(x => x.Id = Guid.NewGuid())
+            .TheFirst(1).With(x => x.UserRoleName = RoleNames.Judge)
+            .TheNext(1).With(x => x.UserRoleName = RoleNames.Individual)
+            .TheNext(1).With(x => x.UserRoleName = RoleNames.Representative)
+            .TheNext(1).With(x => x.UserRoleName = RoleNames.JudicialOfficeHolder)
+            .Build().ToList();
+
+        var judge = participants.Find(x => x.UserRoleName == RoleNames.Judge);
+        var individual = participants.Find(x => x.UserRoleName == RoleNames.Individual);
+        var representative = participants.Find(x => x.UserRoleName == RoleNames.Representative);
+        var judicialOfficeHolder = participants.Find(x => x.UserRoleName == RoleNames.JudicialOfficeHolder);
+        
+        var hearing = Builder<HearingDetailsResponse>.CreateNew()
+            .With(x => x.Id = Guid.NewGuid())
+            .With(x => x.Cases = Builder<CaseResponse>.CreateListOfSize(1).Build().ToList())
+            .With(x => x.Participants = participants).Build();
+
+        var hearingsForNotification = new List<HearingNotificationResponse>
+        {
+            new()
+            {
+                TotalDays = 2,
+                Hearing = hearing
+            }
+        };
+        _mocker.Mock<IBookingsApiClient>().Setup(x => x.GetHearingsForNotificationAsync())
+            .ReturnsAsync(hearingsForNotification);
+            
+        // act
+        await _sut.SendNotificationsAsync();
+            
+        // assert
+        _mocker.Mock<INotificationApiClient>()
+            .Verify(
+                x => x.SendMultiDayHearingReminderEmailAsync(
+                    It.Is<MultiDayHearingReminderRequest>(y => y.ParticipantId == individual.Id)), Times.Once);
+        
+        _mocker.Mock<INotificationApiClient>()
+            .Verify(
+                x => x.SendMultiDayHearingReminderEmailAsync(
+                    It.Is<MultiDayHearingReminderRequest>(y => y.ParticipantId == representative.Id)), Times.Once);
+        
+        _mocker.Mock<INotificationApiClient>()
+            .Verify(
+                x => x.SendMultiDayHearingReminderEmailAsync(
+                    It.Is<MultiDayHearingReminderRequest>(y => y.ParticipantId == judicialOfficeHolder.Id)), Times.Never);
+        
+        _mocker.Mock<INotificationApiClient>()
+            .Verify(
+                x => x.SendMultiDayHearingReminderEmailAsync(
+                    It.Is<MultiDayHearingReminderRequest>(y => y.ParticipantId == judge.Id)), Times.Never);
+    }
+    
+    [Test]
+    public async Task should_send_single_day_reminder_request_for_supported_roles()
+    {
+        // arrange
+        var participants = Builder<ParticipantResponse>.CreateListOfSize(4)
+            .All().With(x => x.Id = Guid.NewGuid())
+            .TheFirst(1).With(x => x.UserRoleName = RoleNames.Judge)
+            .TheNext(1).With(x => x.UserRoleName = RoleNames.Individual)
+            .TheNext(1).With(x => x.UserRoleName = RoleNames.Representative)
+            .TheNext(1).With(x => x.UserRoleName = RoleNames.JudicialOfficeHolder)
+            .Build().ToList();
+
+        var judge = participants.Find(x => x.UserRoleName == RoleNames.Judge);
+        var individual = participants.Find(x => x.UserRoleName == RoleNames.Individual);
+        var representative = participants.Find(x => x.UserRoleName == RoleNames.Representative);
+        var judicialOfficeHolder = participants.Find(x => x.UserRoleName == RoleNames.JudicialOfficeHolder);
+        
+        var hearing = Builder<HearingDetailsResponse>.CreateNew()
+            .With(x => x.Id = Guid.NewGuid())
+            .With(x => x.Cases = Builder<CaseResponse>.CreateListOfSize(1).Build().ToList())
+            .With(x => x.Participants = participants).Build();
+
+        var hearingsForNotification = new List<HearingNotificationResponse>
+        {
+            new()
+            {
+                TotalDays = 1,
+                Hearing = hearing
+            }
+        };
+        _mocker.Mock<IBookingsApiClient>().Setup(x => x.GetHearingsForNotificationAsync())
+            .ReturnsAsync(hearingsForNotification);
+            
+        // act
+        await _sut.SendNotificationsAsync();
+            
+        // assert
+        _mocker.Mock<INotificationApiClient>()
+            .Verify(
+                x => x.SendSingleDayHearingReminderEmailAsync(
+                    It.Is<SingleDayHearingReminderRequest>(y => y.ParticipantId == individual.Id)), Times.Once);
+        
+        _mocker.Mock<INotificationApiClient>()
+            .Verify(
+                x => x.SendSingleDayHearingReminderEmailAsync(
+                    It.Is<SingleDayHearingReminderRequest>(y => y.ParticipantId == representative.Id)), Times.Once);
+        
+        _mocker.Mock<INotificationApiClient>()
+            .Verify(
+                x => x.SendSingleDayHearingReminderEmailAsync(
+                    It.Is<SingleDayHearingReminderRequest>(y => y.ParticipantId == judicialOfficeHolder.Id)), Times.Once);
+        
+        _mocker.Mock<INotificationApiClient>()
+            .Verify(
+                x => x.SendSingleDayHearingReminderEmailAsync(
+                    It.Is<SingleDayHearingReminderRequest>(y => y.ParticipantId == judge.Id)), Times.Never);
+    }
+}
--- SchedulerJobs/SchedulerJobs.Services/HearingNotificationService.cs
+++ SchedulerJobs/SchedulerJobs.Services/HearingNotificationService.cs
@@ -5,28 +5,24 @@ using System.Threading.Tasks;
 using System.Linq;
 using System.Collections.Generic;
 using NotificationApi.Client;
-using SchedulerJobs.Common.Enums;
 using BookingsApi.Contract.V1.Responses;
-using SchedulerJobs.Common.Configuration;
-using SchedulerJobs.Services.Mappers;
+using NotificationApi.Contract.Requests;
 
 namespace SchedulerJobs.Services
 {
     public class HearingNotificationService : IHearingNotificationService
     {
-
         private readonly IBookingsApiClient _bookingsApiClient;
         private readonly INotificationApiClient _notificationApiClient;
         private readonly ILogger<HearingNotificationService> _logger;
-        private readonly List<string> _userRoleList = new List<string>() { "Individual", "Representative", "Judicial Office Holder" };
-        private readonly IFeatureToggles _featureToggles;
+        private readonly List<string> _rolesSupportingSingleDayReminders = new() { RoleNames.Individual, RoleNames.Representative, RoleNames.JudicialOfficeHolder };
+        private readonly List<string> _rolesSupportingMultiDayReminders = new() { RoleNames.Individual, RoleNames.Representative };
         
-        public HearingNotificationService(IBookingsApiClient bookingsApiClient, INotificationApiClient notificationApiClient,  ILogger<HearingNotificationService> logger, IFeatureToggles featureToggles)
+        public HearingNotificationService(IBookingsApiClient bookingsApiClient, INotificationApiClient notificationApiClient,  ILogger<HearingNotificationService> logger)
         {
             _bookingsApiClient = bookingsApiClient;
             _notificationApiClient = notificationApiClient;
             _logger = logger;
-            _featureToggles = featureToggles;
         }
 
         public async Task SendNotificationsAsync()
@@ -42,47 +38,75 @@ namespace SchedulerJobs.Services
             }
 
             await ProcessHearings(hearings);
-
         }
 
         private async Task ProcessHearings(List<HearingNotificationResponse> hearings)
         {
             foreach (var item in hearings)
             {
-                if (!item.Hearing.Participants.Exists(x => _userRoleList.Contains(x.UserRoleName)))
+                if (!item.Hearing.Participants.Exists(x => _rolesSupportingSingleDayReminders.Contains(x.UserRoleName)))
                 {
-                    _logger.LogInformation("SendNotificationsAsync - Ignored hearing: {ItemId} case:{CaseName} as no participant with role required for notification.", item.Hearing.Id, item.Hearing.Cases[0].Name);
+                    _logger.LogInformation("SendNotificationsAsync - Ignored hearing: {ItemId} case:{CaseName} as no participant with role required for notification", item.Hearing.Id, item.Hearing.Cases[0].Name);
                     continue;
                 }
 
-                foreach (var participant in item.Hearing.Participants)
-                {
-                    if (!_userRoleList.Contains(participant.UserRoleName))
-                    {
-                        continue;
-                    }
+                await SendNotificationsForParticipantsInHearing(item);
+            }
+        }
 
-                    await ProcessParticipantForNotification(item, participant);
+        private async Task SendNotificationsForParticipantsInHearing(HearingNotificationResponse item)
+        {
+            foreach (var participant in item.Hearing.Participants)
+            {
+                switch (item.TotalDays)
+                {
+                    case > 1 when _rolesSupportingMultiDayReminders.Contains(participant.UserRoleName):
+                        await ProcessMultiDayHearing(item, participant);
+                        break;
+                    case 1 when _rolesSupportingSingleDayReminders.Contains(participant.UserRoleName):
+                        await ProcessSingleDayHearing(item, participant);
+                        break;
+                    default:
+                        _logger.LogInformation(
+                            "SendNotificationsAsync - Ignored Participant: {ParticipantId} has role {RoleName} which is not supported for notification in the hearing {HearingId}",
+                            participant.Id, participant.UserRoleName, item.Hearing.Id);
+                        continue;
                 }
             }
         }
 
-        private async Task ProcessParticipantForNotification(HearingNotificationResponse item, ParticipantResponse participant)
+        private async Task ProcessMultiDayHearing(HearingNotificationResponse item, ParticipantResponse participant)
         {
-            switch (participant.UserRoleName)
+            await _notificationApiClient.SendMultiDayHearingReminderEmailAsync(new MultiDayHearingReminderRequest()
             {
-                case UserRoleNames.Individual:
-                case UserRoleNames.Representative:
-                case UserRoleNames.JudicialOfficeHolder:
-                    await SendHearingNotification(item, participant);
-                    break;
-            }
+                TotalDays = item.TotalDays,
+                ScheduledDateTime = item.Hearing.ScheduledDateTime,
+                ContactEmail = participant.ContactEmail,
+                Name = $"{participant.FirstName} {participant.LastName}",
+                Username = participant.Username,
+                ParticipantId = participant.Id,
+                HearingId = item.Hearing.Id,
+                CaseName = item.Hearing.Cases[0].Name,
+                CaseNumber = item.Hearing.Cases[0].Number,
+                RoleName = participant.UserRoleName
+            });
         }
 
-        private async Task SendHearingNotification(HearingNotificationResponse item, ParticipantResponse participant)
+        private async Task ProcessSingleDayHearing(HearingNotificationResponse item, ParticipantResponse participant)
         {
-            var request = AddNotificationRequestMapper.MapToHearingReminderNotification(item, participant, _featureToggles.UsePostMay2023Template());
-            await _notificationApiClient.CreateNewNotificationAsync(request);
+            await _notificationApiClient.SendSingleDayHearingReminderEmailAsync(
+                new SingleDayHearingReminderRequest()
+                {
+                    ScheduledDateTime = item.Hearing.ScheduledDateTime,
+                    ContactEmail = participant.ContactEmail,
+                    Name = $"{participant.FirstName} {participant.LastName}",
+                    Username = participant.Username,
+                    ParticipantId = participant.Id,
+                    HearingId = item.Hearing.Id,
+                    CaseName = item.Hearing.Cases[0].Name,
+                    CaseNumber = item.Hearing.Cases[0].Number,
+                    RoleName = participant.UserRoleName
+                });
         }
 
         private async Task<List<HearingNotificationResponse>> GetHearings()
