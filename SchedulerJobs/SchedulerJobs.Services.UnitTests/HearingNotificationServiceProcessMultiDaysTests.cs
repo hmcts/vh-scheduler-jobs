4bc8795ee7128e2f8a0886b071ba1cc63d065abe
Shaed Parkar <shaed.parkar@slimsolutions.tech>
2024-12-03 15:36:15 +0000

    fix post rebase

--- SchedulerJobs/SchedulerJobs.Services.UnitTests/HearingNotificationServiceProcessMultiDaysTests.cs
+++ SchedulerJobs/SchedulerJobs.Services.UnitTests/HearingNotificationServiceProcessMultiDaysTests.cs
@@ -11,7 +11,6 @@ using Moq;
 using NotificationApi.Client;
 using NotificationApi.Contract.Requests;
 using NUnit.Framework;
-using Palmmedia.ReportGenerator.Core.Logging;
 
 namespace SchedulerJobs.Services.UnitTests;
 
@@ -90,7 +89,7 @@ public class HearingNotificationServiceProcessMultiDaysTests
     public async Task should_log_error_for_when_multiday_reminders_request_to_notification_api_fails()
     {
         // arrange
-        var participants = Builder<ParticipantResponse>.CreateListOfSize(4)
+        var participants = Builder<ParticipantResponseV2>.CreateListOfSize(4)
             .All().With(x => x.Id = Guid.NewGuid())
             .TheFirst(1).With(x => x.UserRoleName = RoleNames.Judge)
             .TheNext(1).With(x => x.UserRoleName = RoleNames.Individual)
@@ -98,12 +97,12 @@ public class HearingNotificationServiceProcessMultiDaysTests
             .TheNext(1).With(x => x.UserRoleName = RoleNames.JudicialOfficeHolder)
             .Build().ToList();
         
-        var hearing = Builder<HearingDetailsResponse>.CreateNew()
+        var hearing = Builder<HearingDetailsResponseV2>.CreateNew()
             .With(x => x.Id = Guid.NewGuid())
-            .With(x => x.Cases = Builder<CaseResponse>.CreateListOfSize(1).Build().ToList())
+            .With(x => x.Cases = Builder<CaseResponseV2>.CreateListOfSize(1).Build().ToList())
             .With(x => x.Participants = participants).Build();
 
-        var hearingsForNotification = new List<HearingNotificationResponse>
+        var hearingsForNotification = new List<HearingNotificationResponseV2>
         {
             new()
             {
